1762184a2dccc0d0501b1c5ad52fee0126b0da6c
Ilyas Celik <i.celik@goreha.com>
2025-10-20 18:55:25 +0200

    fix: [CAT-1081] user relative path for navigation

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,7 +1,7 @@
 
 export default new Map([
-["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/developer-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdeveloper-guide.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/cognito-test.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fcognito-test.mdx&astroContentModuleFlag=true")],
+["src/content/docs/developer-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fdeveloper-guide.mdx&astroContentModuleFlag=true")],
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/setup-guide.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fsetup-guide.mdx&astroContentModuleFlag=true")]]);
 		
